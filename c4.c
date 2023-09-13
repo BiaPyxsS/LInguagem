544123036ff43c03915b419650a10cedad33a1c4
BiaPyxsS <133116925+BiaPyxsS@users.noreply.github.com>
2023-09-13 08:25:53 -0300

    Add files via upload

---
+++ c4.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() {
+    char letra;
+    printf("Escreva uma letra \n");
+    scanf ("%c", &letra);
+    if (letra == 'F' || letra == 'f') {
+    printf ("O genero é feminino");
+    } else if (letra == 'M' || letra == 'm') {
+           printf ("O genero é masculino");
+    } else {
+        ("O genero é inválido");
+} }
